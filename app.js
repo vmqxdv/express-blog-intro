4d03db91f5538997023827340aacbf6c742aa8f8
vmqxdv <vquintilio@gmail.com>
2025-03-26 17:51:00 +0100

    feat: Rese pubbliche le immagini

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const PORT = 1229;
 
 
 const posts = [
-  { title: 'Ciambellone', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'images/ciambellone.jpeg', tags: ['ciambella', 'ciambellone', 'semplice'] },
-  { title: 'Cracker Barbabietola', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'images/cracker_barbabietola.jpeg', tags: ['cracker', 'barbabietola', 'leggero'] },
-  { title: 'Pane Fritto Dolce', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'images/pane_fritto_dolce.jpeg', tags: ['pane', 'fritto', 'dolce'] },
-  { title: 'Pasta Barbabietola', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'images/pasta_barbabietola.jpeg', tags: ['pasta', 'barbabietola', 'veloce'] },
-  { title: 'Torta Paesana', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'images/torta_paesana.jpeg', tags: ['torta', 'paesana', 'dolce'] },
+  { title: 'Ciambellone', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'public/images/ciambellone.jpeg', tags: ['ciambella', 'ciambellone', 'semplice'] },
+  { title: 'Cracker Barbabietola', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'public/images/cracker_barbabietola.jpeg', tags: ['cracker', 'barbabietola', 'leggero'] },
+  { title: 'Pane Fritto Dolce', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'public/images/pane_fritto_dolce.jpeg', tags: ['pane', 'fritto', 'dolce'] },
+  { title: 'Pasta Barbabietola', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'public/images/pasta_barbabietola.jpeg', tags: ['pasta', 'barbabietola', 'veloce'] },
+  { title: 'Torta Paesana', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'public/images/torta_paesana.jpeg', tags: ['torta', 'paesana', 'dolce'] },
 ];
 
+
+app.use(express.static('public'));
 
 app.get('/', (_, res) => {
   res.send('Server del mio blog');
